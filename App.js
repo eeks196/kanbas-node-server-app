a5abef6b16940394923c49134537eb50403492e9
eeks196 <seong.d@northeastern.edu>
2024-04-16 21:48:35 -0400

    a6

--- App.js
+++ App.js
@@ -23,6 +23,7 @@ const sessionOptions = {
     sessionOptions.cookie = {
       sameSite: "none",
       secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
     };
   }
   app.use(session(sessionOptions));
