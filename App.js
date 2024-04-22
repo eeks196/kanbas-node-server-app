7c35074986deef8a05db07047df32c9e52c74beb
eeks196 <seong.d@northeastern.edu>
2024-04-22 15:33:10 -0400

    ff

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import cors from "cors";
 import "dotenv/config";
 import UserRoutes from "./Users/routes.js";
 import Lab5 from "./Lab5.js";
-import CourseRoutes from "./Kanbas/Courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 const app = express();
