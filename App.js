ba3ae6e72295d371a2231a0d1c63a03ca1138c7d
eeks196 <seong.d@northeastern.edu>
2024-04-22 15:29:35 -0400

    ff

--- App.js
+++ App.js
@@ -1,39 +1,36 @@
-import express from 'express'
-import mongoose from "mongoose"
-import Hello from "./Hello.js"
-import Lab5 from './Lab5.js'
-import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/modules/routes.js";
-import UserRoutes from './Users/routes.js'
+import express from "express";
+import mongoose from "mongoose";
 import session from "express-session";
+import cors from "cors";
 import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
+import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
-const app = express()
-app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    })
-  );
-  const sessionOptions = {
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
+const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-  };
-  if (process.env.NODE_ENV !== "development") {
+};
+if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
       sameSite: "none",
       secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
     };
-}
+  }
 app.use(session(sessionOptions));  
 app.use(express.json());
-Hello(app)
-Lab5(app)
-CourseRoutes(app)
-ModuleRoutes(app)
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
 UserRoutes(app);
 app.listen(process.env.PORT || 4000);
