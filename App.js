de188c6fd11f1c5d7f743900b6207735a31c3fdf
eeks196 <seong.d@northeastern.edu>
2024-04-22 15:26:54 -0400

    idk

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import express from "express";
-import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/modules/routes.js";
-import dotenv from "dotenv";
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:3000", "https://https://a5--jocular-semifreddo-273014.netlify.app/"],
+}));
 app.use(express.json());
-CourseRoutes(app);
 ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app);
-Hello(app);
 app.listen(process.env.PORT || 4000);
